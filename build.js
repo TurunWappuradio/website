cb8b13f6f2c8597ba1cd14599076bdcdc1e74d96
Konsta Purtsi <kovipu@utu.fi>
2019-03-02 22:29:32 +0200

    Add base for program data fetching Metalsmith component

--- build.js
+++ build.js
@@ -3,18 +3,20 @@ const markdown = require('metalsmith-markdown');
 const filter = require('metalsmith-filter');
 const layouts = require('metalsmith-layouts');
 const handlebarsHelpers = require('./lib/handlebarsHelpers');
+const programDataFetcher = require('./lib/programDataFetcher');
 
 metalsmith(__dirname)
   .source('./src/content/')
   .destination('dist/')
   .use(markdown())
   .use(handlebarsHelpers({'process': process}))
+  .use(programDataFetcher({'process': process}))
   .use(layouts({
-    'engine':'handlebars', 
+    'engine':'handlebars',
     'directory':'src/layouts',
     'partials': 'src/partials'
   }))
-  .use(filter(['!*.scss', '!dist/*', '*.md', '*.html']))
+  .use(filter(['!*.scss', '!dist/*', '*.md', '*.html', 'program-data.json']))
   .build(function(err){
     if(err) { throw err; }
     else console.log('Metalsmith build successfull!');

9188f375634d19b2c90130d6430ec6a914e3f979
Konsta Purtsi <kovipu@utu.fi>
2019-04-24 21:41:32 +0300

    Add metadata form.

---
+++ src/components/MetadataForm/MetadataForm.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MetadataForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Tähän tulee metadata formi.
+      </div>
+    );
+  }
+}
+
+export default MetadataForm;

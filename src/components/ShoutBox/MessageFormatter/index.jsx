fa43aa2b06fc8ad8415ab6f2b9951a539fbdf4f5
Jussi Wallin <jussi.w.wallin@gmail.com>
2020-10-25 11:53:02 +0200

    Fix fixes

--- src/components/ShoutBox/MessageFormatter/index.jsx
+++ src/components/ShoutBox/MessageFormatter/index.jsx
@@ -9,17 +9,13 @@ const MessageFormatter = ({ name, message, color, isAdmin, onBanClick }) => (
     <div
       style={{
         width: isAdmin ? 'calc(100% - 94px)' : '100%',
-        paddingLeft: '0.5rem',
-        paddingBottom: '0.5rem',
+        paddingLeft: '0.5rem'
       }}>
       <NameFormatter name={name} />
       <div className="sbMessageText">{message}</div>
     </div>
     {isAdmin && name !== 'Toimitus' && name !== 'Palvelin' && (
-      <Button title="Bännää" onClick={() => onBanClick(name)}
-        style={{
-          paddingBottom: '0.5rem',
-        }}>
+      <Button title="Bännää" onClick={() => onBanClick(name)}>
         Bännää
       </Button>
     )}

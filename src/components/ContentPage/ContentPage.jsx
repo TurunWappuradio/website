d9cb52b3247a820008ec0c3aac5e478cd37875fd
Konsta Purtsi <konsta@identio.fi>
2020-10-27 10:58:50 +0200

    Add support for embedding videos.

--- src/components/ContentPage/ContentPage.jsx
+++ src/components/ContentPage/ContentPage.jsx
@@ -11,9 +11,24 @@ export default ({ content }) => (
 
 let options = {
   renderNode: {
-    'embedded-asset-block': (node) =>
-      <div className="ImageContainer">
-        <img className="embedded-image" src={node.data.target.fields.file.url + '?w=900'} />
-      </div>
+    'embedded-asset-block': (node) => {
+      const { contentType, url } = node.data.target.fields.file;
+
+      switch (contentType) {
+        case 'image/jpeg':
+        case 'image/png':
+          return (
+            <div className="ImageContainer">
+              <img className="embedded-image" src={url + '?w=900'} />
+            </div>
+          );
+        case 'video/mp4':
+          return (
+            <div className="VideoContainer">
+              <video src={url} controls />
+            </div>
+          )
+      }
+    }
   }
 }

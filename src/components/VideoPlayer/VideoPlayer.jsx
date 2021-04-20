f851159af481876e460d3eaa993c95f6bad60f92
Konsta Purtsi <konsta@identio.fi>
2021-04-20 21:06:34 +0300

    Fix twitch embed.

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -3,6 +3,6 @@ import './VideoPlayer.scss';
 
 export default class extends React.Component {
   render() {
-    return <iframe src="https://player.twitch.tv/?channel=turunwappuradio" />;
+    return <iframe src="https://player.twitch.tv/?channel=turunwappuradio&parent=www.turunwappuradio.com&muted=true" />;
   }
 }
